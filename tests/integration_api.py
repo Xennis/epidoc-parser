5fce70c729c357980deb45f477fac9d1324f8980
Xennis <code@xennis.org>
2020-04-09 22:01:35 +0200

    Setup integration test and concurrent execution

--- tests/integration_api.py
+++ tests/integration_api.py
@@ -1,22 +1,37 @@
+import concurrent.futures
 import glob
 import os
 import unittest
 
+from git import Repo
+
 import epidoc
 
-# Note: git clone https://github.com/papyri/idp.data.git testdata/idp.data
 TESTDATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "testdata", "idp.data")
 
 
 class TestIntegration(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        if os.path.exists(TESTDATA_DIR):
+            return
+        Repo.clone_from("https://github.com/papyri/idp.data.git", TESTDATA_DIR, branch="master", depth=1)
+
+    @staticmethod
+    def load(filename):
+        with open(filename) as f:
+            try:
+                epidoc.load(f)
+            except Exception as e:
+                # Raised exception are ignored by executor.map
+                return Exception(f"{filename} has error {e.__class__.__name__}: {e}")
+
     def load_all(self, path):
         assert os.path.exists(path), f"path {path} exists"
-        for filename in glob.glob(os.path.join(path, "**", "*.xml"), recursive=True):
-            with open(filename) as f:
-                try:
-                    epidoc.load(f)
-                except Exception as e:
-                    self.fail(f"{filename} has error {e.__class__.__name__}: {e}")
+        with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+            for result in executor.map(TestIntegration.load, glob.glob(os.path.join(path, "**", "*.xml"), recursive=True)):
+                if isinstance(result, Exception):
+                    self.fail(result)
 
     def test_apd(self):
         self.load_all(os.path.join(TESTDATA_DIR, "APD"))
